393a06d162b10fb5d019cd33ec3a9e3ab0dfc4de
Adrian Fernandez <adrian.fernandez.i1997@gmail.com>
2018-10-23 21:43:12 +0200

    Creando modelos

--- data/models.py
+++ data/models.py
@@ -2,5 +2,37 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Category(models.Model):
+	name = models.CharField(_("Name"), max_length=128, unique=True, blank=false)
+	slug = models.SlugField(_("Slug"), unique=True)
+	created = models.DateField(_("Date"), default=datetime.date.today)
+	tooltip = models.CharField(_("Tooltip"), max_length=128)
+
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
+
+	class Meta:
+		verbose_name_plural = 'Categories'
+	
+	def __str__(self): # For Python 2, use __unicode__ too
+		return self.name
+
+class Workflow(models.Model):
+	name = models.CharField(max_length=128, unique=True, blank=false)
+	slug = models.SlugField(unique=True)
+	description = models.CharField(max_length=512, default="")
+
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
+
+	class Meta:
+		verbose_name_plural = 'Workflows'
+	
+	def __str__(self): # For Python 2, use __unicode__ too
+		return self.name
